21e100928c70a9d1c226fd2e263177709d529eb3
Time Mao <760135448@qq.com>
2020-01-09 14:41:14 +0800

    Update File_write_to_txt.cpp

--- c++/File_write_to_txt.cpp
+++ c++/File_write_to_txt.cpp
@@ -1,1 +1,10 @@
+// for c
+FILE *imgWrite = fopen("e://landmarks_test//test_nv12//rgb//img_eye_64.txt", "w");
 
+if (!imgWrite) {
+	printf("fopen fail");
+  return 1;
+}
+fprintf(imgWrite, "%d ", *(p + i*imgw + j));
+
+fclose(imgWrite);
